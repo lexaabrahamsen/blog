3b9f7503ae1e68f0bdb0b405ce23fa16bfed7f82
Lexa <lexaraea@gmail.com>
2019-09-08 19:15:46 -0400

    Working edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,19 +9,27 @@ class ArticlesController < ApplicationController
 
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
 
   def create
     @article = Article.new(article_params)
-   
-    @article.save
-    redirect_to @article
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-   
+
 
   private
     def article_params
       params.require(:article).permit(:title, :text)
-  end
+    end
 end
